ccaac58db303f9554887ade3f6fde9e6475577c4
ViniciusLemosM <40804611+viniciusll@users.noreply.github.com>
2022-07-26 20:09:18 -0300

    card

--- src/components/projects/index.tsx
+++ src/components/projects/index.tsx
@@ -1,8 +1,71 @@
 import { Container } from "./styles";
 
+const myProjects = [
+  {
+    name: "Avaliacao-Tecninca-Frontend",
+    description: "In this project I created an e-commerce for front-end technical evaluation.",
+    technologies: [
+      {
+        name: "ReactJs",
+        image: ""
+      }
+    ],
+    image: ""
+  },
+  {
+    name: "Food Delivery",
+    description: "I created this project for me to train my skills with NestJs and to study Prisma.",
+    technologies: [
+      {
+        name: "NestJs",
+        image: ""
+      },
+      {
+        name: "Prisma",
+        image: ""
+      }
+    ],
+    image: ""
+  },
+  {
+    name: "Library api",
+    description: "I created this project to see how to make a backend application with NestJs using hexagonal architecture.",
+    technologies: [
+      {
+        name: "NestJs",
+        image: ""
+      },
+      {
+        name: "Typeorm",
+        image: ""
+      }
+    ],
+    image: ""
+  },
+];
+
 const Projects = () => {
     return (
       <Container data-section="projects" id='projects'>
+        <h1 className="title">Projects</h1>
+        {myProjects.map(project => (
+          <div className="project-container" key={project.name}>
+            <div className="project-image-container">
+              <img src={project.image} alt={project.name} />
+            </div>
+            <div className="project-body">
+              <div className="project">
+                <h2>{project.name}</h2>
+                <p>{project.description}</p>
+              </div>
+              <div className="technologies">
+                {project.technologies.map((technology, i) => (
+                  <img key={i} src={technology.image} alt={technology.name} />
+                ))}
+              </div>
+            </div>
+          </div>
+        ))}
       </Container>
     );
 }
--- src/components/projects/styles.ts
+++ src/components/projects/styles.ts
@@ -2,5 +2,11 @@ import styled from 'styled-components';
 
 export const Container = styled.section`
     height: 100vh;
-    
+    margin-top: 10%;
+    display: flex;
+    flex-direction: column;
+
+    .title {
+        color: '#fff'
+    }
 `;
